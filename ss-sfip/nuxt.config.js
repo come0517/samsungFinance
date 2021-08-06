2e84d381a957ad2ab8cd634d5a5215088a754029
th.lee <th.lee@adcapsule.co.kr>
2021-08-06 15:13:41 +0900

    Octavia scss 추가

--- ss-sfip/nuxt.config.js
+++ ss-sfip/nuxt.config.js
@@ -24,8 +24,13 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~assets/css/common.css'
+    '~assets/css/common.css',
+    '~assets/scss/variables.scss'
   ],
+  buildModules: ['@nuxtjs/style-resources'],
+  styleResources: {
+    less: '~/assets/scss/*.scss'
+  },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
