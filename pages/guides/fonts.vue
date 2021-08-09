f965b052f20dab0a8547b14d320cfaef2c8c1431
jei-lee <iam.mulue@gmail.com>
2021-08-09 20:28:40 +0900

    -

--- pages/guides/fonts.vue
+++ pages/guides/fonts.vue
@@ -8,71 +8,80 @@
         <v-list-item>
           <v-list-item-content>
             <v-list-item-title>상세타이틀 1</v-list-item-title>
-            <strong class="text-h5">통합플랫폼 삼성증권</strong>
+            <strong class="fs-title-1">통합플랫폼 삼성증권</strong>
           </v-list-item-content>
         </v-list-item>
 
         <v-list-item>
           <v-list-item-content>
             <v-list-item-title>상세타이틀 2</v-list-item-title>
-            <strong class="text-h6">통합플랫폼 삼성증권</strong>
+            <strong class="fs-title-2">통합플랫폼 삼성증권</strong>
           </v-list-item-content>
         </v-list-item>
 
         <v-list-item>
           <v-list-item-content>
             <v-list-item-title>대제목 H1</v-list-item-title>
-            <h1>통합플랫폼 삼성증권</h1>
+            <h1 class="fs-h1">통합플랫폼 삼성증권</h1>
           </v-list-item-content>
         </v-list-item>
-        <v-list-item>
+        <v-list-item> 
           <v-list-item-content>
             <v-list-item-title>대제목 H2</v-list-item-title>
-            <h2>통합플랫폼 삼성증권</h2>
+            <h2 class="fs-h2">통합플랫폼 삼성증권</h2>
           </v-list-item-content>
         </v-list-item>
         <v-list-item>
           <v-list-item-content>
             <v-list-item-title>대제목 H3</v-list-item-title>
-            <h3>통합플랫폼 삼성증권</h3>
+            <h3 class="fs-h3">통합플랫폼 삼성증권</h3>
           </v-list-item-content>
         </v-list-item>
 
         <v-list-item>
           <v-list-item-content>
             <v-list-item-title>본문 - 여러줄</v-list-item-title>
-            <p class="text-body1">통합플랫폼 삼성증권</p>
+            <p class="fs-p-ml">통합플랫폼 삼성증권</p>
           </v-list-item-content>
         </v-list-item>
 
         <v-list-item>
           <v-list-item-content>
             <v-list-item-title>본문 - 한줄</v-list-item-title>
-            <p class="text-body2">통합플랫폼 삼성증권</p>
+            <p class="fs-p">통합플랫폼 삼성증권</p>
           </v-list-item-content>
         </v-list-item>
 
         <v-list-item>
           <v-list-item-content>
             <v-list-item-title>본문 - 완료 (강조)</v-list-item-title>
-            <strong class="text-body1">통합플랫폼 삼성증권</strong>
+            <strong class="fs-p-emp">통합플랫폼 삼성증권</strong>
           </v-list-item-content>
         </v-list-item>
 
         <v-list-item>
           <v-list-item-content>
             <v-list-item-title>주석</v-list-item-title>
-            <p class="text-caption">통합플랫폼 삼성증권</p>
+            <p class="fs-cap">통합플랫폼 삼성증권</p>
           </v-list-item-content>
         </v-list-item>
 
         <v-list-item>
           <v-list-item-content>
             <v-list-item-title>폼</v-list-item-title>
-            <p class="text-body1">Forms 에서 상세 확인</p>
+            <p class="fs-form">Forms 에서 상세 확인</p>
             <nuxt-link to="/guides/forms">Forms 바로가기</nuxt-link>
           </v-list-item-content>
         </v-list-item>
+
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>기타</v-list-item-title>
+            <p class="fs-form">통합플랫폼 삼성증권</p>
+          </v-list-item-content>
+        </v-list-item>
+
+
     </v-list>
 
   </section>
--- pages/guides/forms.vue
+++ pages/guides/forms.vue
@@ -1,10 +1,73 @@
 <template>
+<v-form>
   <section class="sfip">
+
+    <h1>fonts</h1>
+    <v-divider class="mb-5"/>
+
+    <v-list>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>폼 기본</v-list-item-title>
+            <v-text-field
+            v-model="firstname"
+            :counter="10"
+            label="이름"
+            placeholder="10자 이내로 입력해주세요."
+            persistent-placeholder=true
+            clearable
+          ></v-text-field>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>폼-필수입력</v-list-item-title>
+            <v-text-field
+            v-model="email"
+            :rules="[rules.required, rules.email]"
+            label="E-mail"
+            placeholder="이메일을 입력해주세요."
+            persistent-placeholder=true
+            clearable
+          ></v-text-field>
+          </v-list-item-content>
+        </v-list-item>
+        <!-- <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>폼-완료문구 있을 경우</v-list-item-title>
+            <v-text-field
+            v-model="firstname"
+            :counter="10"
+            label="기타"
+            placeholder="10자 이내로 입력해주세요."
+            persistent-placeholder=true
+            messages="올바른 입력값입니다."
+            clearable
+          ></v-text-field>
+          </v-list-item-content>
+        </v-list-item> -->
+    </v-list>
+
   </section>
+</v-form>
 </template>
 
 <script>
   export default {
-      layout: 'sfip',
+    layout: 'sfip',
+    data () {
+      return {
+        title: 'Preliminary report',
+        email: '',
+        rules: {
+          required: value => !!value || '필수로 입력해야합니다.',
+          counter: value => value.length <= 30 || '최대 30글자 이내로 입력해주세요.',
+          email: value => {
+            const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            return pattern.test(value) || '올바른 이메일 형식이 아닙니다..'
+          },
+        },
+      }
+    },
   }
 </script>
