1a1bb9d70c30a38056f8e156200bd34b35e6987f
김혜진 <1123kimhj@homeplus.co.kr>
2021-08-24 17:38:48 +0900

    마이자산 메인작업

--- pages/asset/main.vue
+++ pages/asset/main.vue
@@ -16,7 +16,7 @@
                             height="122px"
                         ></v-img> 
                     </div>                        
-                    <v-btn text class="btn">비대면계좌개설 시작하기 <i class="ico"></i></v-btn>
+                    <v-btn text class="btn mt-2">비대면계좌개설 시작하기 <i class="ico"></i></v-btn>
                 </div>
                 <div class="bubble justify-end">
                     <v-img
@@ -58,7 +58,12 @@
         </div>
 
         <div class="cont-wrap">
-            <v-expansion-panels accordion flat class="acco-icon">
+            <v-expansion-panels 
+                v-model="panel" 
+                accordion 
+                flat 
+                class="acco-icon"
+            >
                 <v-expansion-panel>
                     <v-expansion-panel-header>
                         <span class="fs-title-4">
@@ -66,32 +71,32 @@
                             혜택 수수료율
                         </span>
                     </v-expansion-panel-header>
-                    <v-expansion-panel-content>
+                    <v-expansion-panel-content class="pb-15">
                         <dl class="txt-dl">
                             <dt>평생혜택 대상</dt>
                             <dd>
                                 <v-row>
-                                    <v-col>
+                                    <v-col class="tit col-8">
                                         주식 <br>(거래소, 코스닥, 코넥스)
                                     </v-col>
-                                    <v-col>
+                                    <v-col class="col-4">
                                         <div class="txt-percent"><strong>0.00</strong><span>3639</span>%</div>
                                     </v-col>
                                 </v-row>
                                 <v-row>
-                                    <v-col>
+                                    <v-col class="tit col-8">
                                         ETF, ETN
                                     </v-col>
-                                    <v-col >
-                                        <div class="txt-percent"><strong>0.00</strong><span>42087</span>%></div>
+                                    <v-col class="col-4">
+                                        <div class="txt-percent"><strong>0.00</strong><span>42087</span>%</div>
                                     </v-col>
                                 </v-row>
                                 <v-row>
-                                    <v-col>
+                                    <v-col class="tit col-8">
                                         K-OTC
                                     </v-col>
-                                    <v-col >
-                                        <div class="txt-percent"><strong>0.00</strong><span>09187</span>%></div>
+                                    <v-col class="col-4">
+                                        <div class="txt-percent"><strong>0.00</strong><span>09187</span>%</div>
                                     </v-col>
                                 </v-row>
                             </dd>
@@ -100,17 +105,17 @@
                             <dt>평생혜택 비대상</dt>
                             <dd>
                                 <v-row>
-                                    <v-col>
+                                    <v-col class="tit col-8">
                                         주식, ETF, ETN
                                     </v-col>
-                                    <v-col >
-                                        <div class="txt-percent"><strong>0.07</strong><span>7216</span>%></div>
-                                        <div class="txt-percent"><strong>~0.49</strong><span>7216</span>%></div>
+                                    <v-col class="col-4">
+                                        <div class="txt-percent"><strong>0.07</strong><span>7216</span>%</div>
+                                        <div class="txt-percent"><strong>~0.49</strong><span>7216</span>%</div>
                                     </v-col>
                                 </v-row>
                             </dd>
                         </dl>
-                        <ul>
+                        <ul class="bul-list mt-10">
                             <li>자세한 사항 홈페이지 참조</li>
                             <li>혜택 타인승계 불가</li>
                             <li>지점에서 계좌 개설 시 이벤트 대상에서 제외됩니다.</li>
@@ -124,7 +129,7 @@
                             혜택 제공 조건
                         </span>
                     </v-expansion-panel-header>
-                    <v-expansion-panel-content>
+                    <v-expansion-panel-content class="pb-15">
                         <dl class="txt-dl">
                             <dt>기간</dt>
                             <dd><p>2021년 1월 1일(금) ~ 12월 31일(금)</p></dd>
@@ -133,10 +138,10 @@
                             <dt>대상</dt>
                             <dd>
                                 <strong class="fs-h2">신규고객</strong>
-                                <p>주민번호 기준 2021년 1월 이후 최초 신규고객</p>
+                                <p class="mt-3">주민번호 기준 2021년 1월 이후 최초 신규고객</p>
                                 <strong class="fs-h2">휴면 고객</strong>
-                                <p>2018년 1월 1일 ~ 2020년 12월 24일 기간 내 국내주식 거래가 없으면서 2020년 12월 24일 삼성증권 보유 계좌 총 잔고 10만원 이하 고객</p>
-                                <ul>
+                                <p class="mt-3">2018년 1월 1일 ~ 2020년 12월 24일 기간 내 국내주식 거래가 없으면서 2020년 12월 24일 삼성증권 보유 계좌 총 잔고 10만원 이하 고객</p>
+                                <ul class="bul-list mt-5">
                                     <li>2018년 1월 ~ 2020년 12월까지의 신규 고객은 제외</li>
                                 </ul>
                             </dd>
@@ -153,7 +158,7 @@
                             <dt>혜택</dt>
                             <dd>
                                 <p>이벤트 기간 중 개설된 최초 01종합(비대면)계좌(‘평생’ 혜택 표기 계좌)에서 온라인 국내주식 위탁거래 수수료 평생 혜택</p>
-                                <ul>
+                                <ul class="bul-list mt-5">
                                     <li>휴면고객도 이벤트 기간 중 새로 01종합(비대면)계좌 (‘평생’ 혜택 표기 계좌)를 개설하셔야 혜택이 제공됩니다.</li>
                                     <li>이벤트 기간 중 최초 개설된 1개의 비대면 종합계좌에 한하여 수수료 이벤트 적용(1인 1계좌)</li>
                                     <li>평생혜택 비대상 고객 대상 표준수수료율은 주식(ETF)거래 시 0.077216%~0.497216%이며, 기타 자세한 사항은 홈페이지를 참고하시기 바랍니다.</li>
@@ -161,8 +166,8 @@
                             </dd>
                         </dl>
                         
-                        <div class="help-wrap">
-                            <h4>계좌 개설 도움이 필요하신가요?</h4>
+                        <div class="help-wrap mt-10">
+                            <h4 class="fs-title-4">계좌 개설 도움이 필요하신가요?</h4>
                             <p>평생 혜택 전용 상담번호로 연락주세요.</p>
                             <a href="tel:1599-9080"><i class="ico"></i>1599 - 9080</a>
                         </div>
@@ -175,14 +180,14 @@
                             유의사항
                         </span>
                     </v-expansion-panel-header>
-                    <v-expansion-panel-content>
-                        <ul >
+                    <v-expansion-panel-content class="pb-15">
+                        <ul class="bul-list mt-8 type-warning">
                             <li>비대면 계좌 개설 고객에 한하여 온라인 국내 주식 위탁거래 수수료 평생혜택을 제공하며, 삼성증권 지점에서 계좌 개설 시 혜택에서 제외됩니다.</li>
                             <li>평생혜택 계좌는 1인 1계좌만 가능하며, 기타 협의 수수료 혜택 적용이 불가합니다.</li>
                             <li>평생 혜택은 타인승계가 불가합니다.</li>
                         </ul>
-                        <h4 class="fs-title-4">1. 온라인 국내주식 수수료 평생혜택 이벤트 유의사항</h4>
-                        <ul>
+                        <h4 class="fs-title-4 mt-10">1. 온라인 국내주식 수수료 평생혜택 이벤트 유의사항</h4>
+                        <ul class="bul-list type-black">
                             <li>평생혜택 이벤트는 2021년 1월 1일(금) ~ 12월 31일(금)까지 한시적으로 운영되는 이벤트 입니다.</li>
                             <li>법인, 외국인, 미성년자는 이벤트 대상에서 제외됩니다.</li>
                             <li>비대면계좌개설을 통해 개설된 01종합(비대면)계좌(‘평생’ 혜택 표기 계좌)에 한하여 제공되며,  은행 및 지점개설 계좌, 제휴 비대면개설 계좌(네이버페이, 리브메이트 계좌 등), 주식전용,  저축, 랩, 세금우대, 퇴직연금, 코넥스 소액계좌 등 특수계좌는 제외됩니다.</li>
@@ -193,8 +198,8 @@
                             <li>본 이벤트는 당사 사정 또는 금융당국의 지도 및 권고에 의해 사전 고지없이 조기종료될 수 있으며, 이에 따라 추가적인 혜택의 지급이 중단될 수 있습니다. (단, 조기종료 전 이벤트 조건 충족고객 및 당첨고객은 혜택 지급)</li>
                             <li class="font-weight-bold">주식 거래 시 세금은 고객부담입니다. </li>
                         </ul>
-                        <h4 class="fs-title-4">2. 투자 유의사항</h4>
-                        <ul>
+                        <h4 class="fs-title-4 mt-10">2. 투자 유의사항</h4>
+                        <ul class="bul-list type-black">
                             <li>투자자는 금융투자상품에 대하여 증권사로부터 충분한 설명을 받을 권리가 있으며, 투자전 상품설명서 등을 반드시 읽어보시기 바랍니다.</li>
                             <li>금융상품은 예금자보호법에 따라 예금보험공사가 보호하지 않습니다.</li>
                             <li>금융투자상품은 자산가격 변동 등에 따라 투자원금의 손실(0~100%)이 발생할 수 있으며, 그 손실은 투자자에게 귀속됩니다.</li>
@@ -205,7 +210,7 @@
                             <li>주식(ETF)거래 시 표준 수수료는(평생혜택미적용)는 0.077216% ~ 0.497216%이며, 기타 자세한 사항은 홈페이지를 참고하시기 바랍니다. (평생 혜택 계좌는 이벤트 혜택을 참고해주세요.)</li>
                             <li>신용융자 이자율은 영업점/은행연계 계좌는 4.9~9.3%, 비대면 계좌는 5.5~9.9%가 적용됩니다.</li>
                         </ul>
-                        <p class="txt-judge">삼성증권 준법감시인 심사필 제21-B1622호(2021.06.11~2022.06.10)</p>
+                        <p class="txt-judge mt-10">삼성증권 준법감시인 심사필 제21-B1622호(2021.06.11~2022.06.10)</p>
                     </v-expansion-panel-content>
                 </v-expansion-panel>
             </v-expansion-panels>
@@ -214,3 +219,13 @@
     </v-main> 
   </section>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        panel: 0
+      }
+    }
+  }
+</script>
---
+++ pages/asset/main2.vue
@@ -0,0 +1,188 @@
+<template>
+  <section class="sfip">
+    <v-main class="myassets">
+
+        <div class="cont-wrap pb-0">
+            <div class="asset-total">
+                <h2 class="fs-title-4"><strong>삼성증권</strong> 자산</h2>
+                <div class="total mt-2">
+                    <span>20,327,000</span>원
+                    <v-btn icon width="24" color="#ccc"><v-icon>mdi-information-outline</v-icon></v-btn>
+                </div>
+                <div class="break-even minus mt-4">
+                    <span><span>230,489</span>원</span><v-icon small>mdi-menu-up</v-icon><strong>10.31%</strong>
+                </div>
+            </div>
+            <div class="link-wrap">
+                <a href="#none" class="btn">
+                    <span>투자/팔기 신청현황</span>
+                    <v-icon small color="#111">mdi-chevron-right</v-icon>
+                </a>
+            </div>
+        </div>
+
+        <div class="cont-wrap">
+            <v-tabs
+                v-model="tab"
+                class="tab-base"
+                color="#0B0D0F"
+                height="56px"
+            >
+                <v-tab>계좌별</v-tab>
+                <v-tab>유형별</v-tab>
+                <v-tab>상품별</v-tab>
+            </v-tabs>
+            <v-tabs-items v-model="tab">
+                <v-tab-item>
+                    <div class="card-account no-data">
+                        <p>투자 중인 상품이 없습니다</p>
+                    </div>
+                    <div class="card-account mt-5">
+                        <v-card outlined>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <div class="tit-wrap">
+                                        <v-row align="center">
+                                            <v-col>
+                                                <div class="tit"><strong>종합</strong></div>
+                                            </v-col>
+                                            <v-col class="text-right">
+                                                <div class="account-num">7078949809-01</div>
+                                            </v-col>
+                                        </v-row>
+                                    </div>
+                                    <div class="price-info">
+                                        <div class="total"><span>12,568,000</span>원</div>
+                                        <div class="break-even minus">
+                                            <span><span>+230,489</span>원</span><v-icon small>mdi-menu-up</v-icon><strong>10.31%</strong>
+                                        </div>
+                                    </div>
+                                </v-list-item-content>
+                            </v-list-item>
+                            <v-card-actions>
+                                <v-row align="center">
+                                    <v-col class="txt-sub">
+                                        예수금 <span>260,000원</span>
+                                    </v-col>
+                                    <v-col class="text-right">
+                                        <v-btn text>거래내역 <v-icon small>mdi-chevron-right</v-icon></v-btn>
+                                    </v-col>
+                                </v-row>
+                            </v-card-actions>
+                        </v-card>
+                    </div>
+                    <div class="card-account mt-5">
+                        <v-card outlined>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <div class="tit-wrap">
+                                        <v-row align="center">
+                                            <v-col>
+                                                <div class="tit"><strong>종합</strong></div>
+                                            </v-col>
+                                            <v-col class="text-right">
+                                                <div class="account-num">7078949809-01</div>
+                                            </v-col>
+                                        </v-row>
+                                    </div>
+                                    <div class="price-info">
+                                        <div class="total"><span>12,568,000</span>원</div>
+                                        <div class="break-even plus">
+                                            <span><span>+230,489</span>원</span><v-icon small>mdi-menu-down</v-icon><strong>10.31%</strong>
+                                        </div>
+                                    </div>
+                                </v-list-item-content>
+                            </v-list-item>
+                            <v-card-actions>
+                                <v-row align="center">
+                                    <v-col class="txt-sub">
+                                        예수금 <span>260,000원</span>
+                                    </v-col>
+                                    <v-col class="text-right">
+                                        <v-btn text>거래내역 <v-icon small>mdi-chevron-right</v-icon></v-btn>
+                                    </v-col>
+                                </v-row>
+                            </v-card-actions>
+                        </v-card>
+                    </div>
+                </v-tab-item>
+                <v-tab-item>
+                    <div class="card-account mt-5">
+                        <v-card outlined>
+                            <v-list-item>
+                                <v-list-item-content class="pt-6">
+                                    <div class="tit"><strong>[펀드]</strong></div>
+                                    <div class="price-info">
+                                        <div class="total"><span>12,568,000</span>원</div>
+                                        <div class="break-even plus">
+                                            <span><span>+230,489</span>원</span><v-icon small>mdi-menu-down</v-icon><strong>10.31%</strong>
+                                        </div>
+                                    </div>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-card>
+                    </div>
+                </v-tab-item>
+                <v-tab-item>
+                    <div class="card-account mt-5">
+                        <v-card outlined>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <div class="tit-wrap type2">
+                                        <v-row align="center">
+                                            <v-col>
+                                                <div class="tit"><strong>종합N페이</strong></div>
+                                            </v-col>
+                                            <v-col class="text-right">
+                                                <div class="account-num">7078949809-01</div>
+                                            </v-col>
+                                        </v-row>
+                                    </div>
+                                    <div class="tit"><strong>[펀드]</strong> 피델리티글로벌테크놀로지증권자투</div>
+                                    <div class="price-info">
+                                        <div class="total"><span>12,568,000</span>원</div>
+                                        <div class="break-even minus">
+                                            <span><span>+230,489</span>원</span><v-icon small>mdi-menu-down</v-icon><strong>10.31%</strong>
+                                        </div>
+                                    </div>
+                                </v-list-item-content>
+                            </v-list-item>
+
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <div class="tit"><strong>[주식]</strong> 삼성전자</div>
+                                    <div class="price-info">
+                                        <div class="total"><span>12,568,000</span>원</div>
+                                        <div class="break-even plus">
+                                            <span><span>+230,489</span>원</span><v-icon small>mdi-menu-up</v-icon><strong>10.31%</strong>
+                                        </div>
+                                    </div>
+                                </v-list-item-content>
+                            </v-list-item>
+                            
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <div class="tit">예수금</div>
+                                    <div class="price-info">
+                                        <div class="total"><span>25,800</span>원</div>
+                                    </div>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-card>
+                    </div>
+                </v-tab-item>
+            </v-tabs-items>
+        </div>
+    </v-main> 
+  </section>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        tab: null,
+      }
+    },
+  }
+</script>
